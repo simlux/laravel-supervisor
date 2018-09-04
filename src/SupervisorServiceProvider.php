8d0bb7eca9737dcfb30e3512664a200718ba0fba
Simon Kleeschulte <simlux78@gmail.com>
2018-09-04 12:10:06 +0200

    added service provider for package

---
+++ src/SupervisorServiceProvider.php
@@ -0,0 +1,29 @@
+<?php declare(strict_mode=1);
+
+namespace Simlux\LaravelSupervisor;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class SupervisorServiceProvider
+ *
+ * @package Simlux\Supervisor
+ */
+class SupervisorServiceProvider extends ServiceProvider
+{
+    /**
+     * @var bool
+     */
+    protected $defer = true;
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        
+    }
+
+}
